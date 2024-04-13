27b709d63f5ab798f95afda08ae7b68b6c52152a
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-04-13 21:22:42 +0300

    Create Category Controller

--- Models/Category.cs
+++ Models/Category.cs
@@ -8,7 +8,6 @@ namespace EcommerceWebsite.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
-        public string Name { get; set; }
+        public required string Name { get; set; }
     }
 }
