165084d3a4926f6823d39ecb5154eab685860c2a
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-04-26 17:35:03 +0300

    Add data annotations to model properties for validation and relationships

--- Models/Product.cs
+++ Models/Product.cs
@@ -7,6 +7,20 @@ namespace EcommerceWebsite.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public bool IsHome { get; set; }
+        public bool IsStock { get; set; }
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
 
     }
 }
