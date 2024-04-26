759c4470293bf546ec90d48f02249e880ac9b85e
Huri Selenge ÇAĞIN <selengecagin@gmail.com>
2024-04-26 17:34:03 +0300

    Create Id propery and Add Key attribute

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceWebsite.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+    }
+}
